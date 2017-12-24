e23041196f14574e555eadc4bb36ca07c0e642f1
jwowillo <will.orourke10@gmail.com>
2017-12-24 13:28:52 -0600

    Add Template

---
+++ template.go
@@ -0,0 +1,24 @@
+package gen
+
+import (
+	"bytes"
+	"html/template"
+)
+
+// Template is a Golang HTML template.
+type Template struct {
+	Page
+}
+
+// NewTemplate from template files tmpls with x injected into it at path p.
+func NewTemplate(tmpls []string, x interface{}, p string) (*Template, error) {
+	tmpl, err := template.ParseFiles(tmpls...)
+	if err != nil {
+		return nil, err
+	}
+	buf := &bytes.Buffer{}
+	if err := tmpl.Execute(buf, x); err != nil {
+		return nil, err
+	}
+	return &Template{NewPage(p, buf)}, nil
+}
