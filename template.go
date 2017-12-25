2496e17394a085c59f574ca8b9becd7902c3c0e5
jwowillo <will.orourke10@gmail.com>
2017-12-25 00:01:49 -0600

    Fix Template Parsing

--- template.go
+++ template.go
@@ -3,6 +3,7 @@ package gen
 import (
 	"bytes"
 	"html/template"
+	"path/filepath"
 )
 
 // Template is a Golang HTML template.
@@ -12,15 +13,15 @@ type Template struct {
 
 // NewTemplate from template files tmpls with x injected into it at path p.
 func NewTemplate(tmpls []string, x interface{}, p string) (*Template, error) {
-	tmpl, err := template.ParseFiles(tmpls...)
-	tmpl = tmpl.Funcs(template.FuncMap{
+	base := filepath.Base(tmpls[0])
+	tmpl, err := template.New(base).Funcs(template.FuncMap{
 		"inc": func(x int) int { return x + 1 },
-	})
+	}).ParseFiles(tmpls...)
 	if err != nil {
 		return nil, err
 	}
 	buf := &bytes.Buffer{}
-	if err := tmpl.Execute(buf, x); err != nil {
+	if err := tmpl.ExecuteTemplate(buf, base, x); err != nil {
 		return nil, err
 	}
 	return &Template{NewPage(p, buf)}, nil
